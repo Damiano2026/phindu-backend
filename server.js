c5e06dd363dfc4394b840fe31a031246998bcc93
Damiano2026 <damiyelesani@outlook.com>
2026-04-26 09:22:59 +0200

    Refactor server.js for improved readability and structure
    Fix DB SSL connection

--- server.js
+++ server.js
@@ -158,5 +158,6 @@ async function initDB() {
 // Start server AFTER DB init
 
 initDB().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+  app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});;
